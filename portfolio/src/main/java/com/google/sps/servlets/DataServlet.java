757b4b8a45d228513cd2a6e6fe90a7669566544d
Chenai Mangachena <chenaimangachena23@gmail.com>
2020-06-16 10:20:37 -0400

    Adds persistent comments functionality to page using DatastoreService

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,7 +13,12 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
-
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson; 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
@@ -30,18 +36,28 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
+    Query query = new Query("Comment").addSort("fullName", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<String> comments = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      String fullName = (String) entity.getProperty("fullName");
+      String comment = (String) entity.getProperty("comment");
+      comments.add(fullName);
+      comments.add(comment);
+    }
+
     // Converts message to json
     Gson gson = new Gson();
-    String json = gson.toJson(comments);
    
     // Sets the value of what to return as response
     response.setContentType("application/json;");
-    response.getWriter().println(json);
+    response.getWriter().println(gson.toJson(comments));
 
   }
   
-   // Data structure to store names and comments
-      ArrayList<String> comments = new ArrayList();
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
@@ -52,9 +68,14 @@ public class DataServlet extends HttpServlet {
       // Gets comment from form
       String comment = request.getParameter("comment");
 
-      // Populates ArrayList with names and comments
-      comments.add(fullName);
-      comments.add(comment);
+      //Creates comment entity
+      Entity commentEntity = new Entity("Comment");
+
+      commentEntity.setProperty("fullName", fullName);
+      commentEntity.setProperty("comment", comment);
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(commentEntity);
 
       // Redirects to main page
       response.sendRedirect("/index.html");
